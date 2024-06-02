1551911877401891697c40015f1d0674ac6aca92
hamzariffic <hamza.mattea@gmail.com>
2024-06-02 22:20:31 +0300

    Binding was incorrect in the previous code but it's been factored again for both repositories

--- app/src/main/java/com/example/detector/di/AppModule.kt
+++ app/src/main/java/com/example/detector/di/AppModule.kt
@@ -3,13 +3,12 @@ package com.example.detector.di
 import com.example.detector.data.GeminiRepository
 import com.example.detector.data.GeminiRepositoryImpl
 import com.example.detector.domain.usecase.GeminiUseCase
+import com.google.ai.client.generativeai.GenerativeModel
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -32,11 +31,13 @@ abstract class AppModule {
 
         @Provides
         @Singleton
-        fun provideRetrofit(): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl("https://api.gemini.com") // Replace with gemini's actual base URL or figure out how to use the api keys to make these requests
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+        fun provideGenerativeModel(): GenerativeModel {
+            // Access the API key as a Build Configuration variable
+            val apiKey = BuildConfig.apiKey
+            return GenerativeModel(
+                modelName = "gemini-1.5-flash",
+                apiKey = apiKey
+            )
         }
     }
 }
