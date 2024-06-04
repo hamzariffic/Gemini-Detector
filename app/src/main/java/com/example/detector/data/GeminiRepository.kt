86cca3ee962ab7c320be08b38590fa73e32034df
hamzariffic <hamza.mattea@gmail.com>
2024-06-04 07:41:15 +0300

    Implemented the repository eith two suspend functions that can be overriden

--- app/src/main/java/com/example/detector/data/GeminiRepository.kt
+++ app/src/main/java/com/example/detector/data/GeminiRepository.kt
@@ -4,5 +4,5 @@ import com.example.detector.domain.model.GeminiResponse
 import com.example.detector.domain.usecase.GeminiUseCase
 
 interface GeminiRepository : GeminiUseCase {
-    override suspend fun invoke(prompt: String): GeminiResponse
+    override suspend fun invoke(prompt: String, additionalInputs: List<Any>?): GeminiResponse
 }
