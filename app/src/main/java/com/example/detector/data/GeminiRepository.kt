d31a714be135802aa466a2e878090c8acdf7089e
hamzariffic <hamza.mattea@gmail.com>
2024-06-04 07:57:30 +0300

    Updated to match the use case

--- app/src/main/java/com/example/detector/data/GeminiRepository.kt
+++ app/src/main/java/com/example/detector/data/GeminiRepository.kt
@@ -2,7 +2,11 @@ package com.example.detector.data
 
 import com.example.detector.domain.model.GeminiResponse
 import com.example.detector.domain.usecase.GeminiUseCase
+import com.google.ai.client.generativeai.type.GenerationConfig
+import com.google.ai.client.generativeai.type.SafetySetting
+import kotlinx.coroutines.flow.Flow
 
 interface GeminiRepository : GeminiUseCase {
-    override suspend fun invoke(prompt: String, additionalInputs: List<Any>?): GeminiResponse
+    override suspend fun generateContentStream(prompt: String): Flow<GeminiResponse>
+    override suspend fun initializeModel(modelName: String, apiKey: String, generationConfig: GenerationConfig, safetySettings: List<SafetySetting>)
 }
