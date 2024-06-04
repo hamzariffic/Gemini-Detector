0e409d57cdf521530e8f8213f3c48e3e75ce7144
hamzariffic <hamza.mattea@gmail.com>
2024-06-04 07:34:57 +0300

    Updated the useCase with two functions

--- app/src/main/java/com/example/detector/domain/usecase/GeminiUseCase.kt
+++ app/src/main/java/com/example/detector/domain/usecase/GeminiUseCase.kt
@@ -1,8 +1,14 @@
 package com.example.detector.domain.usecase
 
 import com.example.detector.domain.model.GeminiResponse
+import com.google.ai.client.generativeai.type.GenerationConfig
+import com.google.ai.client.generativeai.type.SafetySetting
+import kotlinx.coroutines.flow.Flow
 
+// Use case for interacting with the Gemini API
 interface GeminiUseCase {
-    suspend fun invoke(prompt: String): GeminiResponse
+//    This function handles the asynchronous streaming of responses from the Gemini API. Streaming is necessary
+    suspend fun generateContentStream(prompt: String): Flow<GeminiResponse>
+//    Initializing Gemini model with necessary configurations.
+    suspend fun initializeModel(modelName: String, apiKey: String, generationConfig: GenerationConfig, safetySettings: List<SafetySetting>)
 }
-
