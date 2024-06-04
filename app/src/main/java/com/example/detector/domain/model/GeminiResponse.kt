e2743179697408bc0e57dea654bd0519197834ce
hamzariffic <hamza.mattea@gmail.com>
2024-06-04 07:59:04 +0300

    Gemini Response

--- app/src/main/java/com/example/detector/domain/model/GeminiResponse.kt
+++ app/src/main/java/com/example/detector/domain/model/GeminiResponse.kt
@@ -3,5 +3,8 @@ package com.example.detector.domain.model
 // Data class representing the response from the Gemini API
 data class GeminiResponse(
     val content: String, // The text content of the response will be mostly via text
-    val images: List<String> = emptyList() // List of image URLs (if any)
+    val images: List<String> = emptyList(), // List of image URLs (if any)
+    val videoUrls: List<String> = emptyList(), //Handles video processing
+    val audioUrls: List<String> = emptyList(),//Handles Audio processing
+    val pdfUrls: List<String> = emptyList()//Handles PDFs
 )
